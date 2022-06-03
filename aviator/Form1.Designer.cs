9277b74f52a2e198c95f4bf84019a7570ce32a86
Mehmet Can Özcan <memo-260@hotmail.com>
2022-06-03 03:29:36 +0300

    Add project files.

---
+++ aviator/Form1.Designer.cs
@@ -0,0 +1,345 @@
+﻿namespace aviator
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Segoe UI", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.textBox2.Location = new System.Drawing.Point(402, 360);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(206, 49);
+            this.textBox2.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Gold;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(433, 317);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(146, 31);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Bahis Miktarı";
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button2.Location = new System.Drawing.Point(392, 425);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(229, 42);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Bahsi Yatır Turu Başlat";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Segoe UI", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button3.Location = new System.Drawing.Point(449, 501);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(130, 90);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "BAHİSİ SAT";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(238, 228);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(539, 67);
+            this.progressBar1.TabIndex = 8;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Gold;
+            this.label4.Font = new System.Drawing.Font("Segoe UI", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label4.Location = new System.Drawing.Point(746, 25);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(66, 31);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Kasa:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.Gold;
+            this.label5.Font = new System.Drawing.Font("Segoe UI", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label5.Location = new System.Drawing.Point(839, 25);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(66, 31);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Kasa:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Gold;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label3.Location = new System.Drawing.Point(12, 38);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(84, 31);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Bakiye:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.BackColor = System.Drawing.Color.Gold;
+            this.label6.Font = new System.Drawing.Font("Segoe UI", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label6.Location = new System.Drawing.Point(117, 38);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(146, 31);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "Kalan Bakiye:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Gold;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(543, 65);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(36, 41);
+            this.label1.TabIndex = 13;
+            this.label1.Text = "X";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.BackColor = System.Drawing.Color.Gold;
+            this.label7.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label7.Location = new System.Drawing.Point(421, 65);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(36, 41);
+            this.label7.TabIndex = 14;
+            this.label7.Text = "X";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.BackColor = System.Drawing.Color.PaleGoldenrod;
+            this.label8.Font = new System.Drawing.Font("Segoe UI", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label8.Location = new System.Drawing.Point(238, 660);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(586, 31);
+            this.label8.TabIndex = 15;
+            this.label8.Text = "Bu Program Mehmet Can Özcan Tarafından Kodlanmıştır";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Segoe UI", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button1.Location = new System.Drawing.Point(2, 619);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(194, 51);
+            this.button1.TabIndex = 16;
+            this.button1.Text = "PARA ÇEK";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 574);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(171, 27);
+            this.textBox1.TabIndex = 17;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(182, 114);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(94, 108);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 18;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.BackColor = System.Drawing.Color.Gold;
+            this.label9.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label9.Location = new System.Drawing.Point(182, 239);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(34, 41);
+            this.label9.TabIndex = 19;
+            this.label9.Text = "1";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.BackColor = System.Drawing.Color.Gold;
+            this.label10.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label10.Location = new System.Drawing.Point(790, 239);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(50, 41);
+            this.label10.TabIndex = 20;
+            this.label10.Text = "10";
+            // 
+            // timer2
+            // 
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.BackColor = System.Drawing.Color.Gold;
+            this.label11.Font = new System.Drawing.Font("Segoe UI", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label11.Location = new System.Drawing.Point(746, 396);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(175, 31);
+            this.label11.TabIndex = 21;
+            this.label11.Text = "Sattığın X Oranı";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.BackColor = System.Drawing.Color.Gold;
+            this.label12.Font = new System.Drawing.Font("Segoe UI", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label12.Location = new System.Drawing.Point(746, 446);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(175, 31);
+            this.label12.TabIndex = 22;
+            this.label12.Text = "Sattığın X Oranı";
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Segoe UI", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button4.Location = new System.Drawing.Point(846, 574);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(113, 81);
+            this.button4.TabIndex = 23;
+            this.button4.Text = "yeniden başla";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Navy;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(971, 700);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox2);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "BMX MEHMETATOR";
+            this.TopMost = true;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Timer timer1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Button button4;
+    }
+}
---
+++ aviator/Form1.cs
@@ -0,0 +1,226 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.Threading;
+
+namespace aviator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public static  double kasa = 100000.0;
+        public static double bakiye = 1000.0;
+        public static int bahismiktari = 0;
+        public static int sayi = 0;
+        public static int sayi2 = 0;
+        public static int status = 0;
+
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+         
+           
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            progressBar1.Minimum = 100;
+            double deger2 = progressBar1.Value;
+            deger2 =  (deger2 / 100);
+            deger2  = Math.Round(deger2, 2);
+            label7.Text = "" + deger2;
+            label6.Text = "" + bakiye;
+            label5.Text = "" + kasa;
+
+            
+           
+            progressBar1.Maximum = 1000;
+
+            
+            progressBar1.Increment(1);
+           
+
+            if (progressBar1.Value == sayi)
+            {
+                button3.Visible = false;
+                timer1.Enabled = false;
+                timer2.Enabled = false;
+                button2.Visible = true;
+                pictureBox1.Location = new Point(182, 114);
+                progressBar1.Value = 100;
+                if (status == 0)
+                {
+
+
+                    
+
+
+                }
+            }
+
+
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+            var psi = new ProcessStartInfo
+            {
+                FileName = "https://linktr.ee/mcozcan",
+                UseShellExecute = true,
+            };
+            
+            Process.Start(psi);
+            
+            timer1.Interval = 20; //0.5 saniye
+            timer2.Interval = 60; //0.5 saniye
+            backgroundWorker1.RunWorkerAsync();
+       
+            label6.Text = "" + bakiye;
+            label5.Text = "" + kasa;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (kasa < 0)
+            {
+                MessageBox.Show("Kasayı bitirdin oC yeter! ! ! ! ");
+            }
+
+            status = 0;
+            button2.Visible = false;
+            bahismiktari = Convert.ToInt32(textBox2.Text);
+            if  (bakiye < 1)
+            {
+                MessageBox.Show("Paran bitmiş siktir git");
+               
+            } 
+
+            else
+            {
+                if (bakiye < bahismiktari)
+                {
+                    MessageBox.Show("Paran Kadar oyna amınakodum badigarları çağırtma bana");
+                    button2.Visible = true;
+                }
+
+                else
+                {
+
+                    kasa = kasa + bahismiktari;
+                    bakiye = bakiye - bahismiktari;
+                    pictureBox1.Location = new Point(182, 114);
+
+
+
+                    
+                    Random rastgele = new Random();
+
+                    sayi2 = rastgele.Next(1, 10);
+                 
+                    if(sayi2 == 1 || sayi2 == 2 || sayi2 == 3)
+                    {
+                        sayi = rastgele.Next(100, 199);
+
+                    }
+
+                    if (sayi2 == 4 || sayi2 == 5 || sayi2 == 6)
+                    {
+                        sayi = rastgele.Next(100, 299);
+
+                    }
+
+                    if (sayi2 == 7)
+                    {
+
+
+                        sayi = rastgele.Next(100, 599);
+
+
+                    }
+
+                    if (sayi2 == 8)
+                    {
+
+
+                        sayi = rastgele.Next(100, 799);
+
+
+                    }
+
+                    if (sayi2 == 9)
+                    {
+                    
+                        
+                            sayi = rastgele.Next(100, 999);
+                        
+
+                    }
+                    
+
+
+                   
+
+                 
+
+
+                    timer1.Enabled = true;
+                    timer2.Enabled = true;
+                    button3.Visible = true;
+
+                    
+                }
+            }
+
+
+
+           
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            button3.Visible = false;
+            double oran = progressBar1.Value;
+            double oran2 = oran / 100;
+            bakiye = bakiye + (bahismiktari * (oran2));
+            kasa = kasa - bahismiktari*oran2;
+            status = 1;
+            label12.Text = "" + oran2;
+
+            
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Form2 ff = new Form2();
+            ff.Show();
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            pictureBox1.Location = new Point(pictureBox1.Location.X + 1, 114);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            this.Controls.Clear();
+            this.InitializeComponent();
+            kasa = 100000;
+            bakiye = 1000;
+            label6.Text = "" + bakiye;
+            label5.Text = "" + kasa;
+        }
+    }
+}
